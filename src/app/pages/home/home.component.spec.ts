b2ef0a5b2c93f390e527598db2145a9d005df2d0
anyelinash <anyelina.sosa@tecsup.edu.pe>
2024-11-10 16:34:46 -0500

    creacion de carpetas

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ValidacionComponent } from './validacion.component';
+import { HomeComponent } from './home.component';
 
-describe('ValidacionComponent', () => {
-  let component: ValidacionComponent;
-  let fixture: ComponentFixture<ValidacionComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ValidacionComponent]
+      imports: [HomeComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ValidacionComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
