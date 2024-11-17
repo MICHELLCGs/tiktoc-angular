d1321e87c21d394e0facd8950744a2f3b3b1eaed
MICHELLCGs <michell.condori@tecsup.edu.pe>
2024-11-17 18:40:33 -0500

    formularios cambios finales

--- src/app/pages/tiktok/home/home.component.ts
+++ src/app/pages/tiktok/home/home.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import {MessageSurveyComponentsComponent} from '../../../components/message-survey-components/message-survey-components.component'
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MessageSurveyComponentsComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
